bee4d80e7a8f05ba4c98c1b4f79f331dcdbc3357
amuhtarov <amuhtarov@akumoproject.com>
2024-01-20 11:52:30 -0600

    test

--- vpc.tf
+++ vpc.tf
@@ -46,20 +46,24 @@ resource "aws_route_table" "pub_route" {
 resource "aws_route_table" "priv_route" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    nat_gateway_id = aws_nat_gateway.app_nat_gateway.id
+    cidr_block = var.route_pub
+  }
   tags = {
     Name = "app-private-rt1"
   }
 }
 
 resource "aws_route_table_association" "public" {
-  count = 4
-  subnet_id      = aws_subnet.public_subnets[count.index].id
+  count = count.index
+  subnet_id      = [aws_subnet.public_subnets[count.index].id]
   route_table_id = aws_route_table.pub_route.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = 4
-  subnet_id      = aws_subnet.private_subnets[count.index].id
+  count = count.index
+  subnet_id      = [aws_subnet.private_subnets[count.index].id]
   route_table_id = aws_route_table.priv_route.id
 }
 
@@ -111,7 +115,79 @@ resource "aws_network_acl" "main" {
 
 # Associate NACL with Public Subnets
 resource "aws_network_acl_association" "public_nacl_association" {
-  count = 4
-  subnet_id       = aws_subnet.public_subnets[count.index].id
+  count = count.index
+  subnet_id       = [aws_subnet.public_subnets[count.index].id]
   network_acl_id  = aws_network_acl.main.id
 }
+
+# Create a NAT Gateway
+resource "aws_nat_gateway" "app_nat_gateway" {
+  count = count.index
+  subnet_id     = [aws_subnet.app_public_subnets[0].id] # Associate it with one of the public subnets
+  tags = {
+    Name = "app-natgateway1"
+  } 
+}
+
+resource "aws_network_acl" "priv" {
+  vpc_id = aws_vpc.main.id
+  subnet_ids = [aws_subnet.private_subnets.id]
+
+  # Inbound Rules
+  ingress {
+    rule_no    = 100
+    action     = "allow"
+    protocol   = "-1"
+    cidr_block = aws_vpc.main.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    rule_no    = 110
+    action     = "allow"
+    protocol   = "tcp"
+    cidr_block = aws_vpc.main.cidr_block
+    from_port  = 22
+    to_port    = 22
+  }
+
+  # Outbound Rules
+  egress {
+    rule_no    = 200
+    action     = "allow"
+    protocol   = "-1"
+    cidr_block = var.route_pub
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    rule_no    = 210
+    action     = "allow"
+    protocol   = "tcp"
+    cidr_block = var.route_pub
+    from_port  = 80
+    to_port    = 443
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_s3_bucket" "main" {
+  bucket = "my-sep-homework-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_flow_log" "example" {
+  log_destination = aws_s3_bucket.main.id
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+}
+
