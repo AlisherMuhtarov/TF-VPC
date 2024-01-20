f1c356340aedfb4897b02c0e5ca875f3ce88b811
amuhtarov <amuhtarov@akumoproject.com>
2024-01-20 13:41:22 -0600

    test

--- vpc.tf
+++ vpc.tf
@@ -66,10 +66,8 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_route_table_association" "private" {
-  count = 4
-
-  subnet_id      = aws_subnet.private_subnets[count.index].id
-  route_table_id = aws_route_table.priv_route[count.index].id
+  subnet_id      = aws_subnet.private_subnets.id
+  route_table_id = aws_route_table.priv_route.id
 }
 
 
